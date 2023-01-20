9857963bbf8d6fc0f8bcacfb4947d4d73c6b7450
Игорь Зырянов <i.zyryanov@s7.ru>
2023-01-20 22:51:21 +0300

    Initial

---
+++ src/main/java/com/meshgroup/meshgroupdemo/MeshGroupDemoApplication.java
@@ -0,0 +1,13 @@
+package com.meshgroup.meshgroupdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeshGroupDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeshGroupDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meshgroup/meshgroupdemo/MeshGroupDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meshgroup.meshgroupdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeshGroupDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
